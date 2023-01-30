718e9932c027d20d10b1bc694820286aaa97a198
kaanboyacii <98668706+kaanboyacii@users.noreply.github.com>
2023-01-30 14:26:08 +0300

    Multiple layouts setup 3

--- react/src/router.jsx
+++ react/src/router.jsx
@@ -1,10 +1,10 @@
-import { createBrowserRouter } from "react-router-dom";
+import { createBrowserRouter, Navigate } from "react-router-dom";
 import Login from "./views/Login.jsx";
 import Signup from "./views/Signup.jsx";
 import Users from "./views/Users.jsx";
 import NotFound from "./views/NotFound.jsx";
-import DefaultLayout from "./components/DefaultLayout.js";
-import GuestLayout from "./components/GuestLayout.js";
+import DefaultLayout from "./components/DefaultLayout.jsx";
+import GuestLayout from "./components/GuestLayout.jsx";
 import Dashboard from "./views/Dashboard.jsx";
 
 const router = createBrowserRouter([
@@ -12,6 +12,10 @@ const router = createBrowserRouter([
         path: "/",
         element: <DefaultLayout />,
         children: [
+            {
+                path:'/',
+                element:<Navigate to="/users" />
+            },
             {
                 path: "/dashboard",
                 element: <Dashboard />,
