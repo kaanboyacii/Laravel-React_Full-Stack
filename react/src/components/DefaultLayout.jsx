8530f17902a05d7ca25fa99f7fd8d134784f571d
kaanboyacii <98668706+kaanboyacii@users.noreply.github.com>
2023-02-18 12:24:29 +0300

    Create Default Layout

--- react/src/components/DefaultLayout.jsx
+++ react/src/components/DefaultLayout.jsx
@@ -1,17 +1,40 @@
 import React from "react";
 import { Navigate, Outlet } from "react-router-dom";
-import {useStateContext} from "../contexts/ContextProvider.jsx";
+import { useStateContext } from "../contexts/ContextProvider.jsx";
 
 export default function DefaultLayout() {
-    const {user, token} =useStateContext();
+    const { user, token } = useStateContext();
 
-    if(!token){
-        return <Navigate to="/login"/>
+    if (!token) {
+        return <Navigate to="/login" />;
     }
 
     return (
-        <div>
-            <Outlet />
+        <div id="defaultLayout">
+        <aside>
+          <Link to="/dashboard">Dashboard</Link>
+          <Link to="/users">Users</Link>
+        </aside>
+        <div className="content">
+          <header>
+            <div>
+              Header
+            </div>
+  
+            <div>
+              {user.name} &nbsp; &nbsp;
+              <a onClick={onLogout} className="btn-logout" href="#">Logout</a>
+            </div>
+          </header>
+          <main>
+            <Outlet/>
+          </main>
+          {notification &&
+            <div className="notification">
+              {notification}
+            </div>
+          }
         </div>
+      </div>
     );
 }
