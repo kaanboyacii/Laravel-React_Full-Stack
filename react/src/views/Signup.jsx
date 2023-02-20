8d141e9c64502788ac7e041642ea5b82deba942e
kaanboyacii <98668706+kaanboyacii@users.noreply.github.com>
2023-02-20 12:20:55 +0300

    Signup page

--- react/src/views/Signup.jsx
+++ react/src/views/Signup.jsx
@@ -1,7 +1,29 @@
-import React from 'react'
+import React from "react";
+import { Link } from "react-router-dom";
 
 export default function Users() {
-  return (
-    <div>Sign up</div>
-  )
+    const onSubmit = (ev) => {
+        ev.preventDefault();
+    };
+
+    return (
+        <div className="login-signup-form animated fadeInDown">
+            <div className="form">
+                <form onSubmit={onSubmit}>
+                    <h1 className="title">Sign up for free</h1>
+                    <input placeholder="Full Name" type="text" />
+                    <input placeholder="Email Address" type="email" />
+                    <input placeholder="Password" type="password" />
+                    <input
+                        placeholder="Password Confirmation"
+                        type="password"
+                    />
+                    <button className="btn btn-block">Sign Up</button>
+                    <p className="message">
+                        Already Registered ? <Link to="/login">Sign in</Link>
+                    </p>
+                </form>
+            </div>
+        </div>
+    );
 }
