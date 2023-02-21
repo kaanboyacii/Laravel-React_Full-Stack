b6688c5a85a46d8ba261c77dfb02b4afbb10c26b
kaanboyacii <98668706+kaanboyacii@users.noreply.github.com>
2023-02-21 14:25:40 +0300

    Users Crud in api

--- react/src/views/Users.jsx
+++ react/src/views/Users.jsx
@@ -1,6 +1,26 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import axiosClient from "../axios-client.js";
 
 export default function Users() {
+  const [users, setUsers] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const {setNotification} = useStateContext()
+
+  useEffect(() => {
+    getUsers();
+  }, [])
+
+  const onDeleteClick = user => {
+    if (!window.confirm("Are you sure you want to delete this user?")) {
+      return
+    }
+    axiosClient.delete(`/users/${user.id}`)
+      .then(() => {
+        setNotification('User was successfully deleted')
+        getUsers()
+      })
+  }
+
   return (
     <div>Users</div>
   )
